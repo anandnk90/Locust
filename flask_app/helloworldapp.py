c9146192f1b19e24260ecd421df6cece030c34f1
Anand Nandakumar <anandnk90@gmail.com>
2020-06-15 16:45:29 -0400

    Initial commit

---
+++ flask_app/helloworldapp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Docstring"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """Function"""
+    return 'Hello!'
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
