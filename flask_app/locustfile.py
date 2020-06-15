ce39501cfb625c73c6d6419e2dd0578ddae0d042
Anand Nandakumar <anandnk90@gmail.com>
2020-06-15 18:29:35 -0400

    Final

--- flask_app/locustfile.py
+++ flask_app/locustfile.py
@@ -1,15 +1,9 @@
 """Import locust"""
-from locust import HttpUser, TaskSet, between
+from locust import HttpUser, TaskSet, task, between
 
-@task
-def index_page(self):
-    self.client.get("/")
+class QuickstartUser(HttpUser):
+    wait_time = between(5, 9)
 
-class UserBehavior(TaskSet):
-    """Define taskset"""
-    tasks = {index : 1}
-
-class WebsiteUser(HttpUser):
-    """WebsiteUser simulation"""
-    task_set = UserBehavior
-    wait_time = between(5.0, 9.0)
+    @task
+    def index_page(self):
+        self.client.get("/")
